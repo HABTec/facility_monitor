fdc10c8eb15cd585ef48263c01f2e26dc4fd79bc
Redet Getachew <redetg@gmail.com>
2023-08-29 00:05:17 +0300

    added moment js relative time

--- src/components/DataElementRow.js
+++ src/components/DataElementRow.js
@@ -2,7 +2,7 @@ import React from "react";
 import { DataTableRow, DataTableCell, CircularLoader } from "@dhis2/ui";
 import { useState, useEffect } from "react";
 import { useDataQuery, useDataEngine } from "@dhis2/app-runtime";
-
+import moment from "moment";
 const UsersQuery = {
   orgUnitUsers: ({ orgUnit }) => ({
     resource: `users`,
@@ -37,29 +37,7 @@ const userActivityLog = {
 };
 
 const timeAgo = (prevDate) => {
-  const diff = Number(new Date()) - prevDate;
-  const minute = 60 * 1000;
-  const hour = minute * 60;
-  const day = hour * 24;
-  const month = day * 30;
-  const year = day * 365;
-  switch (true) {
-    case diff < minute:
-      const seconds = Math.round(diff / 1000);
-      return `${seconds} ${seconds > 1 ? "seconds" : "second"} ago`;
-    case diff < hour:
-      return Math.round(diff / minute) + " minutes ago";
-    case diff < day:
-      return Math.round(diff / hour) + " hours ago";
-    case diff < month:
-      return Math.round(diff / day) + " days ago";
-    case diff < year:
-      return Math.round(diff / month) + " months ago";
-    case diff > year:
-      return Math.round(diff / year) + " years ago";
-    default:
-      return "";
-  }
+  return moment(prevDate).fromNow();  
 };
 const findLastLogin = (users) => {
   var maxPeriod = undefined;
@@ -152,7 +130,6 @@ const DataElementRow = ({
       //     .then((data) => {
       //       internal_roles[i].frequency=data?.userActivityLog?.pager?.total;
       //     });
-
       // }
 
       setRoles(internal_roles);
