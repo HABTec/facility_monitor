74c54d2c8dc690fca4270c636443c889f661273d
Redet Getachew <redetg@gmail.com>
2023-09-06 14:28:25 +0300

    closes #14 user activity pane

--- src/components/UserActivityTable.js
+++ src/components/UserActivityTable.js
@@ -18,12 +18,14 @@ import {
   Table,
   DataTableRow,
   DataTableBody,
-  Tooltip
+  InputField,
+  Tooltip,
+  IconSearch24,
+  Button,
 } from "@dhis2/ui";
 
 import { useDataQuery, useDataEngine } from "@dhis2/app-runtime";
 import moment from "moment";
-
 
 const timeAgo = (prevDate) => {
   return moment(prevDate).fromNow();
@@ -32,16 +34,32 @@ const timeAgo = (prevDate) => {
 const UsersQuery = {
   InactiveUsers: {
     resource: `users`,
-    params: ({ orgUnit, includeDisabled, page, pageSize, lastYear }) => {
+    params: ({
+      orgUnit,
+      includeDisabled,
+      page,
+      pageSize,
+      lastYear,
+      filters,
+    }) => {
       let filter = [`userCredentials.lastLogin:lt:${lastYear.toISOString()}`];
       if (orgUnit) filter.push(`organisationUnits.id:eq:${orgUnit}`);
 
       if (!includeDisabled)
         filter.push(`userCredentials.disabled:eq:${includeDisabled}`);
 
+      if (filters.name?.trim() != "")
+        filter.push(`name:ilike:${filters.name.trim()}`);
+      if (filters.username?.trim() != "")
+        filter.push(`userCredentials.username:ilike:${filters.username.trim()}`);
+      if (filters.role?.trim() != "")
+        filter.push(`userCredentials.userRoles.name:ilike:${filters.role.trim()}`);
+      if (filters.phoneNumber?.trim() != "")
+        filter.push(`phoneNumber:ilike:${filters.phoneNumber.trim()}`);
+
       return {
         fields: [
-          "id,name,phoneNumber,userCredentials[username,disabled,lastLogin,userRoles[id,displayName]]",
+          "id,name,phoneNumber,userCredentials[username,phoneNumber,disabled,lastLogin,userRoles[id,displayName]]",
         ],
         filter,
         total: true,
@@ -64,10 +82,16 @@ const UserActivityTable = ({ selectedOrgUnit }) => {
   const [totalPages, setTotalPages] = useState(1);
   const [users, setUsers] = useState();
 
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [role, setRole] = useState("");
+
+  const [refetcher, setRefetcher] = useState();
+
   const engine = useDataEngine();
 
   const handelLoadComplete = (data) => {
-    // console.log(data, "data log");
     setPageCount(data?.InactiveUsers?.pager?.pageCount ?? 1);
     setPageUser(data?.InactiveUsers?.pager?.page ?? 1);
     setPageSize(data?.InactiveUsers?.pager?.pageSize ?? 10);
@@ -82,21 +106,44 @@ const UserActivityTable = ({ selectedOrgUnit }) => {
       pageSize,
       page: pageUser,
       lastYear,
+      filters: { username, phoneNumber, name, role },
     },
     onComplete: handelLoadComplete,
   });
 
+  const filterChanged = (ev) => {
+    console.log(ev);
+    switch (ev.name) {
+      case "name":
+        setName(ev.value);
+        break;
+
+      case "username":
+        setUsername(ev.value);
+        break;
+
+      case "phone":
+        setPhoneNumber(ev.value);
+        break;
+
+      case "role":
+        setRole(ev.value);
+        break;
+    }
+  };
+
   useEffect(() => {
-    let log = userquery
+    userquery
       .refetch({
         orgUnit: selectedOrgUnit?.id ?? null,
         includeDisabled,
         pageSize,
         page: pageUser,
         lastYear,
+        filters: { username, phoneNumber, name, role },
       })
       .then(handelLoadComplete);
-  }, [selectedOrgUnit?.id, includeDisabled, pageUser, pageSize]);
+  }, [selectedOrgUnit?.id, includeDisabled, pageUser, pageSize, refetcher]);
 
   const rows = users?.map((el) => (
     <DataTableRow key={el?.id}>
@@ -107,18 +154,24 @@ const UserActivityTable = ({ selectedOrgUnit }) => {
       <DataTableCell key={el?.id + "2"}>{el.phoneNumber}</DataTableCell>
       <DataTableCell key={el?.id + "5"}>
         {el.userCredentials?.userRoles?.map((role) => (
-          <Chip key={role.id} dense>
+          <Chip key={role.id} dense onClick={()=>{setRole(role.displayName);setRefetcher((prev)=>!prev)}}>
             {role.displayName}
           </Chip>
         ))}
       </DataTableCell>
       <DataTableCell key={el?.id + "3"}>
-        <Tooltip content={el?.userCredentials?.lastLogin}> {timeAgo(new Date(el.userCredentials?.lastLogin).getTime())} </Tooltip>
+        <Tooltip content={el?.userCredentials?.lastLogin}>
+          {timeAgo(new Date(el.userCredentials?.lastLogin).getTime())}
+        </Tooltip>
       </DataTableCell>
     </DataTableRow>
   ));
 
-  console.log(userquery, pageUser);
+  const handelEnterKey = (e,b) => {
+    if (b.key === "Enter") {
+      setRefetcher((prev) => !prev);
+    }
+  };
 
   return (
     <div
@@ -127,22 +180,66 @@ const UserActivityTable = ({ selectedOrgUnit }) => {
         marginRight: "auto",
         minWidth: spacers.dp384,
       }}
-    >
-      <Help>
+    > 
+    <div style={{marginBottom:spacers.dp8}}>
+    <Help>
         The table shows users who are inactive for more than one year.
       </Help>
-      <Switch
-        checked={includeDisabled}
-        label="Include disabled accounts"
-        name="Include disabled accounts"
-        onChange={(e) => {
-          setIncludeDisabled(e.checked);
-        }}
-        className={classes.spaced}
-      />
+    </div>
 
       <DataTable>
         <TableHead>
+          <DataTableRow>
+            <DataTableColumnHeader>
+              <InputField
+                name="name"
+                placeholder="Search Name"
+                value={name}
+                onChange={filterChanged}
+                onKeyDown={handelEnterKey}
+              />
+            </DataTableColumnHeader>
+            <DataTableColumnHeader>
+              <InputField
+                name="username"
+                placeholder="Search username"
+                value={username}
+                onChange={filterChanged}
+                onKeyDown={handelEnterKey}
+              />
+            </DataTableColumnHeader>
+            <DataTableColumnHeader>
+              <InputField
+                name="phone"
+                type="number"
+                placeholder="Search phone number"
+                value={phoneNumber}
+                onChange={filterChanged}
+                onKeyDown={handelEnterKey}
+              />
+            </DataTableColumnHeader>
+            <DataTableColumnHeader>
+              <InputField
+                name="role"
+                placeholder="Search role"
+                onChange={filterChanged}
+                onKeyDown={handelEnterKey}
+                value={role}
+              />
+              <Switch
+                checked={includeDisabled}
+                label="Include disabled accounts"
+                name="Include disabled accounts"
+                onKeyDown={handelEnterKey}
+                onChange={(e) => {
+                  setIncludeDisabled(e.checked);
+                }}
+              />
+            </DataTableColumnHeader>
+            <DataTableColumnHeader>
+              <Button icon={<IconSearch24 />} onClick={()=>{setRefetcher((prev) => !prev)}}/>
+            </DataTableColumnHeader>
+          </DataTableRow>
           <DataTableRow>
             <DataTableColumnHeader>Name</DataTableColumnHeader>
             <DataTableColumnHeader>Username</DataTableColumnHeader>
